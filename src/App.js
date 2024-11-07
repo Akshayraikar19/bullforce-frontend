1eae54d264a56e80020c0a8959cddacff9162ae4
Akshayraikar19 <143585057+Akshayraikar19@users.noreply.github.com>
2024-11-07 16:13:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import Login from './components/Login';
 import VerifyOtp from './components/VerifyOtp';
-import bullLogo from "./images/Group 42228.png"
+import bullLogo from "./images/Group 42228.png";
 
 function App() {
   return (
     <div>
       {/* Navbar */}
-      <Navbar color="dark" dark expand="md" style={{ padding: '5px 20px', height: "68px" }}> 
-        <NavbarBrand href="/" style={{ fontSize: '22px', fontWeight: 'bold' }}> 
-          <img
-            src={bullLogo}
-            alt="Bull Force Logo"
-            style={{ width: '146.82px', height: '45px', Top:"12px", Left: "62px" }} 
+      <Navbar color="dark" dark expand="md" style={{ padding: '5px 20px', height: "68px" }}>
+        {/* Render NavbarBrand only on /verifyOtp route */}
+        <Routes>
+          <Route
+            path="/verifyOtp"
+            element={
+              <NavbarBrand href="/" style={{ fontSize: '22px', fontWeight: 'bold' }}>
+                <img
+                  src={bullLogo}
+                  alt="Bull Force Logo"
+                  style={{ width: '146.82px', height: '45px', top: "12px", left: "62px" }} // Adjust logo size and margin
+                />
+              </NavbarBrand>
+            }
           />
-        </NavbarBrand>
+        </Routes>
+
         <Nav className="ml-auto" navbar>
           <NavItem>
             <NavLink tag={Link} to="/"></NavLink> {/* Add text to NavLink */}
@@ -33,3 +42,4 @@ function App() {
 }
 
 export default App;
+
