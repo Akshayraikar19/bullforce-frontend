31cca9328e429266992eb62b072593b163210059
Akshayraikar19 <143585057+Akshayraikar19@users.noreply.github.com>
2024-11-11 11:04:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,4 @@
 
-// import { Routes, Route, Link } from 'react-router-dom';
-// import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
-// import Login from './components/Login';
-// import VerifyOtp from './components/VerifyOtp';
-// import bullLogo from "./images/Group 42228.png"
-
-// function App() {
-//   return (
-//     <div>
-//       {/* Navbar */}
-//       <Navbar color="dark" dark expand="md" style={{ padding: '5px 20px', height: "68px" }}> {/* Reduce padding */}
-//         <NavbarBrand href="/" style={{ fontSize: '22px', fontWeight: 'bold' }}> {/* Increase font size and make text bold */}
-//           <img
-//             src={bullLogo}
-//             alt="Bull Force Logo"
-//             style={{ width: '146.82px', height: '45px', Top:"12px", Left: "62px" }} // Adjust logo size and margin
-//           />
-//         </NavbarBrand>
-//         <Nav className="ml-auto" navbar>
-//           <NavItem>
-//             <NavLink tag={Link} to="/"></NavLink> {/* Add text to NavLink */}
-//           </NavItem>
-//         </Nav>
-//       </Navbar>
-
-//       {/* Routes */}
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/verifyOtp" element={<VerifyOtp />} />
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import { Routes, Route, Link } from 'react-router-dom';
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import Login from './components/Login';
